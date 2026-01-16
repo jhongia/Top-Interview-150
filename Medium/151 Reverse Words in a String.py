2890191b30fa645e0aaa0684d4498b119637315f
Jhon <jhonjairo0897@hotmail.com>
2026-01-16 17:15:42 -0500

    Leetcode 151. Reverse Words in a String - Medium

---
+++ Medium/151 Reverse Words in a String.py
@@ -0,0 +1,62 @@
+'''
+Docstring for Easy.151 Reverse Words in a String
+151. Reverse Words in a String
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+ 
+
+Example 1:
+
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+Example 2:
+
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+Example 3:
+
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+There is at least one word in s.
+ 
+
+Follow-up: If the string data type is mutable in your language, can you solve it in-place with O(1) extra space?
+'''
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        sentence = ""
+        temp = ""
+        for i in range(len(s)-1, -1, -1):
+            if s[i] != " ":
+                temp += s[i]
+            elif s[i] == " " and temp != "":
+                sentence += temp[::-1]
+                sentence += " "
+                temp = ""
+            if i == 0 and temp != "":
+                sentence += temp[::-1]
+        return sentence.strip()
+
+sol = Solution()
+s = "the sky is blue"
+print(sol.reverseWords(s)) # "blue is sky the"
