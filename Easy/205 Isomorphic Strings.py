cd526d41bee664068023dc4cb965f7f47ae76c96
Jhon <jhonjairo0897@hotmail.com>
2026-02-16 17:15:21 -0500

    Leetcode 205. Isomorphic Strings - Easy.

---
+++ Easy/205 Isomorphic Strings.py
@@ -0,0 +1,72 @@
+'''
+Docstring for Easy.205 Isomorphic Strings
+205. Isomorphic Strings
+Attempted
+Easy
+Topics
+premium lock icon
+Companies
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+ 
+
+Example 1:
+
+Input: s = "egg", t = "add"
+
+Output: true
+
+Explanation:
+
+The strings s and t can be made identical by:
+
+Mapping 'e' to 'a'.
+Mapping 'g' to 'd'.
+Example 2:
+
+Input: s = "f11", t = "b23"
+
+Output: false
+
+Explanation:
+
+The strings s and t can not be made identical as '1' needs to be mapped to both '2' and '3'.
+
+Example 3:
+
+Input: s = "paper", t = "title"
+
+Output: true
+
+ 
+
+Constraints:
+
+1 <= s.length <= 5 * 104
+t.length == s.length
+s and t consist of any valid ascii character.
+
+'''
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        out = False
+        d = {}
+        for i in range(len(s)):
+            if s[i] not in d: 
+                if t[i] not in d.values():
+                    d[s[i]] = t[i]
+                    out = True
+                else:
+                    return False
+            elif d[s[i]] != t[i]:
+                return False
+        return out
+    
+s = "badc"
+t = "baba"
+print(Solution().isIsomorphic(s, t)) # true
