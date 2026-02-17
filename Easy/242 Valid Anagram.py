4b003763d5398069f22b2ff08bbbdc97dd3597dc
Jhon <jhonjairo0897@hotmail.com>
2026-02-17 16:33:20 -0500

    Leetcode 242. Valid Anagram - Easy.

---
+++ Easy/242 Valid Anagram.py
@@ -0,0 +1,44 @@
+'''
+Docstring for Easy.242 Valid Anagram242. Valid Anagram
+Easy
+Topics
+premium lock icon
+Companies
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+
+Output: false
+
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+ 
+
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+'''
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s = ''.join(sorted(s))
+        t = ''.join(sorted(t))
+        if s == t:
+            return True
+        return False
+    
+s = "anagram"
+t = "nagaram"
+print(Solution().isAnagram(s, t)) # True
