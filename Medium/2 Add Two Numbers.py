06f708525e3d92767042f5ab70548f3c52bfd8f2
Jhon <jhonjairo0897@hotmail.com>
2026-03-03 16:52:44 -0500

    Leetcode 2. Add Two Numbers - Medium,

---
+++ Medium/2 Add Two Numbers.py
@@ -0,0 +1,77 @@
+'''
+2. Add Two Numbers
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+
+Example 1:
+
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+Example 2:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+Example 3:
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+ 
+
+Constraints:
+
+The number of nodes in each linked list is in the range [1, 100].
+0 <= Node.val <= 9
+It is guaranteed that the list represents a number that does not have leading zeros.
+'''
+
+#Definition for singly-linked list.
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def createLinkedList(lst):
+    nodes = []
+    for num in lst:
+        nodes.append(ListNode(num))
+
+    for i in range(len(nodes) - 1):
+        nodes[i].next = nodes[i+1]
+    
+    return nodes[0]
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        num1 = ""
+        num2 = ""
+        while l1:
+            num1 = str(l1.val) + num1
+            l1 = l1.next
+        while l2:
+            num2 = str(l2.val) + num2
+            l2 = l2.next
+        
+        suml1l2 = int(num1) + int(num2)        
+        sumList = list(map(int, str(suml1l2)[::-1]))
+
+        return createLinkedList(sumList)
+    
+sol = Solution()
+l1 = createLinkedList([2,4,3])
+l2 = createLinkedList([5,6,4])
+result = sol.addTwoNumbers(l1, l2)
+while result:
+    print(result.val)
+    result = result.next
