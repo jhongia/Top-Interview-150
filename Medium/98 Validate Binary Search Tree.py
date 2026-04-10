bf3ced05ebc67b4bf0c6916cd822445d2fe04587
Jhon <jhonjairo0897@hotmail.com>
2026-04-10 17:11:33 -0400

    Leetcode 98. Validate Binary Search Tree - Medium.

---
+++ Medium/98 Validate Binary Search Tree.py
@@ -0,0 +1,64 @@
+'''
+98. Validate Binary Search Tree
+Attempted
+Medium
+Topics
+premium lock icon
+Companies
+Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+A valid BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys strictly less than the node's key.
+The right subtree of a node contains only nodes with keys strictly greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+ 
+
+Example 1:
+
+
+Input: root = [2,1,3]
+Output: true
+Example 2:
+
+
+Input: root = [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-231 <= Node.val <= 231 - 1
+'''
+
+from typing import List, Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def is_valid(node, minn, maxx):
+            if not node:
+                return True
+
+            if node.val <= minn or node.val >= maxx:
+                return False
+
+            return is_valid(node.left, minn, node.val) and is_valid(node.right, node.val, maxx)
+
+        return is_valid(root, float('-inf'), float('inf'))
+
+root = TreeNode(5)
+root.left = TreeNode(1)
+root.right = TreeNode(4)
+root.right.left = TreeNode(3)
+root.right.right = TreeNode(6)
+
+print(Solution().isValidBST(root)) # false
