8369f4b94421f72a0a1a081a808ed73e63aa8765
Jhon <jhonjairo0897@hotmail.com>
2026-05-11 11:57:25 -0400

    Leetcode 373. Find K Pairs with Smallest Sums - Medium.

---
+++ Medium/373 373. Find K Pairs with Smallest Sums.py
@@ -0,0 +1,66 @@
+'''
+373. Find K Pairs with Smallest Sums
+Medium
+Topics
+premium lock icon
+Companies
+You are given two integer arrays nums1 and nums2 sorted in non-decreasing order and an integer k.
+
+Define a pair (u, v) which consists of one element from the first array and one element from the second array.
+
+Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+Output: [[1,2],[1,4],[1,6]]
+Explanation: The first 3 pairs are returned from the sequence: [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+Example 2:
+
+Input: nums1 = [1,1,2], nums2 = [1,2,3], k = 2
+Output: [[1,1],[1,1]]
+Explanation: The first 2 pairs are returned from the sequence: [1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 105
+-109 <= nums1[i], nums2[i] <= 109
+nums1 and nums2 both are sorted in non-decreasing order.
+1 <= k <= 104
+k <= nums1.length * nums2.length
+'''
+
+from typing import List
+from heapq import heappush, heappop
+
+class Solution:
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        heap = [[nums1[0] + nums2[0], 0, 0]]
+        res = []
+        visited = set((0,0))
+        while heap and k > 0:
+            _, i1, i2 = heappop(heap)
+            res.append([nums1[i1], nums2[i2]])
+            k -= 1
+
+            next_i = i1 + 1
+            if next_i < len(nums1) and (next_i, i2) not in visited:
+                heappush(heap, [nums1[next_i] + nums2[i2], next_i, i2])
+                visited.add((next_i, i2))
+
+            next_i = i2 + 1
+            if next_i < len(nums2) and (i1, next_i) not in visited:
+                heappush(heap, [nums1[i1] + nums2[next_i], i1, next_i])
+                visited.add((i1, next_i))
+            
+        return res
+    
+nums1 = [1,7,11]
+nums2 = [2,4,6]
+k = 3
+
+print(Solution().kSmallestPairs(nums1, nums2, k)) # [[1,2],[1,4],[1,6]]
+
