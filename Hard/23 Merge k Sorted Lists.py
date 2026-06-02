23f00f7a51a9d25afd4bf2ac0fa01a2bbc621a4d
Jhon <jhonjairo0897@hotmail.com>
2026-06-02 12:49:05 -0400

    Leetcode 23. Merge k Sorted Lists - Hard.

---
+++ Hard/23 Merge k Sorted Lists.py
@@ -0,0 +1,88 @@
+'''
+23. Merge k Sorted Lists
+Hard
+Topics
+premium lock icon
+Companies
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted linked list:
+1->1->2->3->4->4->5->6
+Example 2:
+
+Input: lists = []
+Output: []
+Example 3:
+
+Input: lists = [[]]
+Output: []
+ 
+
+Constraints:
+
+k == lists.length
+0 <= k <= 104
+0 <= lists[i].length <= 500
+-104 <= lists[i][j] <= 104
+lists[i] is sorted in ascending order.
+The sum of lists[i].length will not exceed 104.
+'''
+
+from typing import List, Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+import heapq
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        heap = []
+
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, i, node))
+
+        D = ListNode()
+        cur = D
+
+        while heap:
+            val, i, node = heapq.heappop(heap)
+            cur.next = node
+            cur = node
+            node = node.next
+            if node:
+                heapq.heappush(heap, (node.val, i, node))
+
+        return D.next
+
+lists = [[1,4,5],[1,3,4],[2,6]]
+nodes = []
+for l in lists:
+    head = ListNode(l[0])
+    cur = head
+    for i in range(1, len(l)):
+        cur.next = ListNode(l[i])
+        cur = cur.next
+    nodes.append(head)
+res = Solution().mergeKLists(nodes)
+while res:
+    print(res.val, end='->' if res.next else '')
+    res = res.next
+# 1->1->2->3->4->4->5->6
