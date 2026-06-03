a81cf4c9d8816d90e76fa0a2771449a09d8e737e
Jhon <jhonjairo0897@hotmail.com>
2026-06-03 12:09:02 -0400

    Leetcode 295. Find Median from Data Stream - Hard.

---
+++ Hard/295 Find Median from Data Stream.py
@@ -0,0 +1,117 @@
+'''
+295. Find Median from Data Stream
+Hard
+Topics
+premium lock icon
+Companies
+The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value, and the median is the mean of the two middle values.
+
+For example, for arr = [2,3,4], the median is 3.
+For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+Implement the MedianFinder class:
+
+MedianFinder() initializes the MedianFinder object.
+void addNum(int num) adds the integer num from the data stream to the data structure.
+double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+ 
+
+Example 1:
+
+Input
+["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+[[], [1], [2], [], [3], []]
+Output
+[null, null, null, 1.5, null, 2.0]
+
+Explanation
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.addNum(2);    // arr = [1, 2]
+medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+medianFinder.addNum(3);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0
+ 
+
+Constraints:
+
+-105 <= num <= 105
+There will be at least one element in the data structure before calling findMedian.
+At most 5 * 104 calls will be made to addNum and findMedian.
+ 
+
+Follow up:
+
+If all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+If 99% of all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+'''
+
+class MedianFinder:
+
+    def __init__(self):
+        self.md = []
+
+    def addNum(self, num: int) -> None:
+        self.md.append(num)
+
+    def findMedian(self) -> float:
+        n = len(self.md)
+        self.md.sort()
+        if n % 2 != 0:
+            return self.md[int(n//2)]
+        return (self.md[int(n/2)-1] + self.md[int(n/2)]) / 2
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
+
+# Input
+# ["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+# [[], [1], [2], [], [3], []]
+# Output
+# [null, null, null, 1.5, null, 2.0]
+
+# Explanation
+
+["MedianFinder","addNum","findMedian","addNum","findMedian","addNum",
+ "findMedian","addNum","findMedian","addNum","findMedian","addNum",
+ "findMedian","addNum","findMedian","addNum","findMedian","addNum",
+ "findMedian","addNum","findMedian","addNum","findMedian"]
+
+[[],[6],[],[10],[],[2],[],[6],[],[5],[],[0],[],[6],[],[3],[],[1],[],[0],[],[0],[]]
+
+medianFinder = MedianFinder()
+print(medianFinder.addNum(6))
+print(medianFinder.findMedian())
+print(medianFinder.addNum(10))
+print(medianFinder.findMedian())
+print(medianFinder.addNum(2))
+
+print(medianFinder.findMedian())
+print(medianFinder.addNum(6))
+print(medianFinder.findMedian())
+print(medianFinder.addNum(5))
+print(medianFinder.findMedian())
+print(medianFinder.addNum(0))
+
+print(medianFinder.findMedian())
+print(medianFinder.addNum(6))
+print(medianFinder.findMedian())
+print(medianFinder.addNum(3))
+print(medianFinder.findMedian())
+print(medianFinder.addNum(1))
+
+print(medianFinder.findMedian())
+print(medianFinder.addNum(0))
+print(medianFinder.findMedian())
+print(medianFinder.addNum(0))
+print(medianFinder.findMedian())
+
+
+# medianFinder = MedianFinder()
+# print(medianFinder.addNum(1))    # arr = [1]
+# print(medianFinder.addNum(2))    # arr = [1, 2]
+# print(medianFinder.findMedian()) # return 1.5 (i.e., (1 + 2) / 2)
+# print(medianFinder.addNum(3))    # arr[1, 2, 3]
+# print(medianFinder.findMedian()) # return 2.0
